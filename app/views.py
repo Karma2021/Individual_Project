52c7a21968b09b04c724e13b07972c90471f86c2
Karma2021 <lamak0068@gmail.com>
2022-07-25 17:27:45 +0545

    view file added

--- app/views.py
+++ app/views.py
@@ -1,9 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from product.models import Product
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
-def index(request):
-    return render(request, 'index.html')
 
+
+def index(request):
+    product=Product.objects.all().order_by('-id')[0:3]
+    dis=Product.objects.all().order_by('-id')[0:1]
+    print(product)
+    return render(request,'index.html', {'product':product,'dis':dis})
+    
 def about(request):
     return render(request, 'about.html')
 
